d04b86db9e5c056fd77e3c5a6ed337898d9db2dc
Sabina Bariyeva <badalova.s@gmail.com>
2025-07-01 00:29:17 +0300

    Fix css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,25 +1,31 @@
-import Head from 'next/head'
-import Script from 'next/script'
+import '../styles/globals.css'
+// pages/_app.jsx
+import Script from 'next/script';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTX72HPYM6" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
+      {/* Подключаем скрипт GA */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RTX72HPYM6"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
               gtag('config', 'G-RTX72HPYM6');
-            `,
-          }}
-        />
-      </Head>
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
